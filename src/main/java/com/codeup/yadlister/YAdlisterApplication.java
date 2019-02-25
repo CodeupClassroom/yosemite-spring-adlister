5a8f0493a1e3c122e4490dc61c64e31401b8a9a1
fer <fernandoza@gmail.com>
2019-02-25 09:27:37 -0600

    first commit

---
+++ src/main/java/com/codeup/yadlister/YAdlisterApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.yadlister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YAdlisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YAdlisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/yadlister/YAdlisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.yadlister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YAdlisterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
