54e843b654d9f95982f9a1e6dced2b8feec7bbc4
RyanOrsinger <ryan.orsinger@gmail.com>
2019-02-27 15:46:12 -0600

    feat: edit/update posts, add seeder

--- src/main/java/com/codeup/yadlister/controllers/PostController.java
+++ src/main/java/com/codeup/yadlister/controllers/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.GeneratedValue;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class PostController {
         this.postDao = postDao;
     }
 
+    // show all posts
     @GetMapping("/posts")
     public String all(Model model){
 
@@ -26,6 +28,7 @@ public class PostController {
         return "posts/index";
     }
 
+    // show a specific post
     @GetMapping("/posts/{id}")
     public String show(@PathVariable long id, Model model){
 
@@ -36,11 +39,13 @@ public class PostController {
         return "posts/show";
     }
 
+    // load the form for new blog posts
     @GetMapping("/posts/create")
     public String showForm(){
         return "posts/create";
     }
 
+    // save the new post to the database
     @PostMapping("/posts/create")
     public String create(
             @RequestParam(name = "title") String title,
@@ -55,9 +60,52 @@ public class PostController {
 
     }
 
+    // Bring up a post object in a form to edit it.
+    @GetMapping("/posts/{id}/edit")
+    public String edit(@PathVariable long id, Model model) {
+        Post post = postDao.findOne(id);
+        model.addAttribute("post", post);
+        return "posts/edit";
+
+    }
+
+    // Save the edited post object
+    @PostMapping("/posts/{id}/edit")
+    public String update(
+            @PathVariable long id,
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "body") String body,
+            Model model) {
+
+        System.out.println("inside the update method now...");
+
+        Post post = postDao.findOne(id);
+        post.setTitle(title);
+        post.setBody(body);
+
+        postDao.save(post);
+
+        return "redirect:/posts/" + id;
+    }
+
+
+    // delete a specific post
+    @PostMapping("/posts/delete")
+    public String delete(@RequestParam(name = "id") long id) {
+        Post post = postDao.findOne(id);
+        postDao.delete(post);
+
+        return "redirect:/posts";
+    }
+
+    // show a random post
     @GetMapping("/posts/random")
     public String random(Model model) {
 
+        Post post = postDao.getRandom();
+        model.addAttribute("post", post);
+
         return "posts/show";
     }
+
 }
--- src/main/java/com/codeup/yadlister/repositories/PostRepository.java
+++ src/main/java/com/codeup/yadlister/repositories/PostRepository.java
@@ -1,7 +1,15 @@
 package com.codeup.yadlister.repositories;
 
 import com.codeup.yadlister.models.Post;
+import com.codeup.yadlister.quotes.Quote;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
+
+    // get a post at random
+    @Query(value="SELECT * FROM posts ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Post getRandom();
+
+
 }
