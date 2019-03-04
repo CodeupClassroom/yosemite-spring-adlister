df2f3c917372c5684144cdcd2ad91113896843a0
fer <fernandoza@gmail.com>
2019-03-04 09:26:00 -0600

    feat: add services basic structure

---
+++ src/main/java/com/codeup/yadlister/services/EmailService.java
@@ -0,0 +1,18 @@
+package com.codeup.yadlister.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+
+}
