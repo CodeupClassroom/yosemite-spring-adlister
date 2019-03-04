f77e0997bd92c80edc419ba60ae6d18bee0310d1
fer <fernandoza@gmail.com>
2019-03-04 15:24:20 -0600

    feat: finish the relationships lecture

--- src/main/java/com/codeup/yadlister/models/Ad.java
+++ src/main/java/com/codeup/yadlister/models/Ad.java
@@ -1,9 +1,11 @@
 package com.codeup.yadlister.models;
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="ads")
 public class Ad {
+
     @Id @GeneratedValue
     private long id;
 
@@ -13,18 +15,39 @@ public class Ad {
     @Column(nullable = false, length = 2048)
     private String description;
 
-    public Ad(long id, String title, String description) {
+    @OneToOne
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ad")
+    private List<AdImage> images;
+
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "ads_categories",
+            joinColumns={@JoinColumn(name = "ad_id")},
+            inverseJoinColumns={@JoinColumn(name = "category_id")}
+    )
+    private List<Category> categories;
+
+    public Ad(long id, String title, String description, User user, List<AdImage> images, List<Category> categories) {
         this.id = id;
         this.title = title;
         this.description = description;
+        this.user = user;
+        this.images = images;
+        this.categories = categories;
     }
 
     public Ad() {
     }
 
-    public Ad(String title, String description) {
+    public Ad(String title, String description, User user, List<AdImage> images, List<Category> categories) {
         this.title = title;
         this.description = description;
+        this.user = user;
+        this.images = images;
+        this.categories = categories;
     }
 
     public long getId() {
@@ -50,4 +73,29 @@ public class Ad {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<AdImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<AdImage> images) {
+        this.images = images;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
 }
+
