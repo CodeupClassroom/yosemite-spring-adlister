342b48eaecd72df0382ef5267b8612bc6aad4eac
Fernando Mendoza R <fernandoza@gmail.com>
2019-04-29 16:54:06 -0500

    test: unit tests on controllers

--- src/main/java/com/codeup/yadlister/repositories/Users.java
+++ src/main/java/com/codeup/yadlister/repositories/Users.java
@@ -2,7 +2,9 @@ package com.codeup.yadlister.repositories;
 
 import com.codeup.yadlister.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Users extends CrudRepository<User, Long> {
     User findByUsername(String username);
 }
--- src/test/java/com/codeup/yadlister/AdControllerUnitTest.java
+++ src/test/java/com/codeup/yadlister/AdControllerUnitTest.java
@@ -3,20 +3,26 @@ package com.codeup.yadlister;
 import com.codeup.yadlister.controllers.AdController;
 import com.codeup.yadlister.models.Ad;
 import com.codeup.yadlister.repositories.AdRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -25,26 +31,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AdController.class)
-public class AdControllerTests {
+public class AdControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private AdRepository adRepository;
 
     @InjectMocks
     private AdController adController;
 
     @Autowired
-    private MockMvc mockMvc;
+    ApplicationContext context;
 
-    @MockBean
-    private AdRepository adRepository;
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     // After the Ad is being created the controller responds with a redirect
     @Test
-    public void testAddAdHappyPath() throws Exception {
-        Ad newAd = new Ad();
-        newAd.setTitle("MacBook");
-        newAd.setDescription("like new");
-        mockMvc.perform(post("/ads/create", newAd))
+    public void testSaveAdHappyPath() throws Exception {
+        Ad mockAd = new Ad();
+        mockAd.setTitle("MacBook");
+        mockAd.setDescription("like new");
+
+        Mockito.when(adRepository.save(Mockito.any(Ad.class))).thenReturn(mockAd);
+
+        mockMvc.perform(post("/ads/create", mockAd))
         .andExpect(status().isOk())
-        .andExpect(redirectedUrl("/ads"));
+        .andExpect(redirectedUrl("/ads"))
+        .andReturn();
     }
 
 }
